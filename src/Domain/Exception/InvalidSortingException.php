615648c3f85773ccdc74104874cd99c1119c8166
kuba-end <kostek660i@gmail.com>
2025-11-18 14:43:35 +0100

    Improve exceptions handling

--- src/Domain/Exception/InvalidSortingException.php
+++ src/Domain/Exception/InvalidSortingException.php
@@ -4,9 +4,17 @@ declare(strict_types=1);
 
 namespace App\Domain\Exception;
 
-use Exception;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Throwable;
 
-class InvalidSortingException extends Exception
+class InvalidSortingException extends BadRequestHttpException
 {
+    public const EXCEPTION_MESSAGE = 'Invalid sorting argument provided';
 
+    public function __construct(
+        string $message = self::EXCEPTION_MESSAGE,
+
+    ) {
+        parent::__construct($message);
+    }
 }
--- src/Infrastructure/Listener/QueryExceptionListener.php
+++ src/Infrastructure/Listener/QueryExceptionListener.php
@@ -5,10 +5,9 @@ declare(strict_types=1);
 namespace App\Infrastructure\Listener;
 
 use App\Domain\Exception\InvalidSortingException;
-use Doctrine\ORM\Query\QueryException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
-use Symfony\Component\Messenger\Exception\ExceptionInterface;
+use Symfony\Component\Messenger\Exception\HandlerFailedException;
 
 class QueryExceptionListener
 {
@@ -16,27 +15,28 @@ class QueryExceptionListener
     {
         $e = $event->getThrowable();
 
+        if ($e instanceof HandlerFailedException && $e->getPrevious() !== null) {
+            $e = $e->getPrevious();
+        }
+
         if ($e instanceof InvalidSortingException) {
             $response = new JsonResponse([
                 'errors' => [
                     [
                         'status' => '400',
-                        'title' => 'Invalid field in sort/filter',
-                        'detail' => $e->getMessage()
+                        'title' => 'Invalid sorting',
+                        'detail' => $e->getMessage(),
                     ]
                 ]
             ], 400);
 
             $event->setResponse($response);
-        }
-
-        if ($e instanceof ExceptionInterface) {
+        } else {
             $response = new JsonResponse([
                 'errors' => [
                     [
                         'status' => '500',
                         'title' => 'Internal server error',
-                        'detail' => $e->getMessage()
                     ]
                 ]
             ], 500);
